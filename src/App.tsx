4c849313aeb7163bfaebc7a15904b2f43d042227
GKing <jacky2code@gmail.com>
2023-05-05 15:56:30 +0800

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@
  * @Description:
  * @TODO:
  */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Robot from "./components/Robot";
 import styles from "./App.module.css";
 import logo from "./assets/images/logo.svg";
@@ -18,36 +18,42 @@ interface State {
   robotGallery: any[];
 }
 
-class App extends React.Component<Props, State> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      robotGallery: [],
-    };
-  }
+const App: React.FC = (props) => {
+  const [count, setCount] = useState<number>(0);
+  const [robotGallery, setRobotGallery] = useState<any>([])
 
-  componentDidMount() {
+  useEffect(() => {
+    document.title = `点击${count}次`
+  }, [count]);
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => this.setState({ robotGallery: data }));
-  }
+    .then(response => response.json())
+    .then(data => setRobotGallery(data))
+  }, []);
 
-  render() {
-    return (
-      <div className={styles.app}>
-        <div className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h1>React-Typescript-Travelling</h1>
-        </div>
-        <ShoppingCart />
-        <div className={styles.robotList}>
-          {this.state.robotGallery.map((r) => (
-            <Robot id={r.id} name={r.name} email={r.email} />
-          ))}
-        </div>
+  return (
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>React-Typescript-Travelling</h1>
       </div>
-    );
-  }
-}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click
+      </button>
+      <span>count: {count}</span>
+      <ShoppingCart />
+      <div className={styles.robotList}>
+        {robotGallery.map((r) => (
+          <Robot id={r.id} name={r.name} email={r.email} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
